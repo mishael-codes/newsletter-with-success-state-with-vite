e36bdee0bac2aaa65a2e471989fb30b4ab131421
mishael-codes <enyimishael22@gmail.com>
2025-05-28 08:07:39 +0100

    Chore: update README.md, package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
+  base: "/newsletter-with-success-state-with-vite/",
   plugins: [tailwindcss()],
 });
